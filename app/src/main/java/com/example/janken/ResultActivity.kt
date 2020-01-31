ac045df59186a13459a0491de638f7ecc0ed3db3
mimura <musyu36@gmail.com>
2020-01-31 10:57:16 +0900

    選択した手を結果画面に伝播

--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,11 +2,21 @@ package com.example.janken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        //インテントからデータの取り出し, get???Extraでデータ型に対応する
+        val id = intent.getIntExtra("MY_HAND",0)
+
+        when(id){
+            //各リソースidと一致したものを表示
+            R.id.gu -> myHandImage.setImageResource(R.drawable.gu)
+            R.id.choki -> myHandImage.setImageResource(R.drawable.choki)
+            R.id.pa -> myHandImage.setImageResource(R.drawable.pa)
+        }
     }
 }
